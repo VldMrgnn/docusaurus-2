ff4394233a5ac558c4701dec087aa5f66e96b1d3
Vlad <vladmarginean@softwiz.ro>
2023-05-02 09:32:58 +0300

    nop

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,9 +35,6 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: "/",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
         theme: {
@@ -46,7 +43,7 @@ const config = {
       }),
     ],
   ],
-  themes: ['@docusaurus/theme-search-algolia'],
+  // themes: ['@docusaurus/theme-search-algolia'],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -82,119 +79,6 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     })
-    // ({
-    //   navbar: {
-    //     title: 'Început',
-    //     logo: {
-    //       alt: 'Jurnal Gold Logo',
-    //       src: 'img/jurnalgold-icon-64x64.svg',
-    //     },
-    //     items: [
-    //       {
-    //         type: 'doc',
-    //         docId: 'intro',
-    //         position: 'left',
-    //         label: 'Documentație',
-    //       },
-    //       {
-    //         href: 'https://admin.jurnalgold.ro',
-    //         label: 'JGAP',
-    //         position: 'left',
-            
-    //       },
-    //       // {to: '/blog', label: 'Blog', position: 'left', enabled:false},
-    //       {
-    //         href: 'https://github.com/SoftwizCreation/Jurnal-Gold',
-    //         label: 'GitHub',
-    //         position: 'right',
-    //       },
-    //     ],
-    //   },
-    //   footer: {
-    //     style: 'dark',
-    //     links: [
-    //       {
-    //         title: 'Docs',
-    //         items: [
-    //           {
-    //             label: 'Tutorial',
-    //             to: '/docs/intro',
-    //           },
-    //         ],
-    //       },
-    //       {
-    //         title: 'Pagina de Web',
-    //         items: [
-    //           {
-    //             label: 'Softwiz, contact',
-    //             href: 'https://www.softwiz.ro/contact/',
-    //           }
-    //         ],
-    //       },
-    //       {
-    //         title: 'Altele',
-    //         items: [
-    //           {
-    //             label: 'Blog',
-    //             to: '/blog',
-    //           },
-    //           // {
-    //           //   label: 'Email:',
-    //           //   href: 'mailto:support@softwiz.ro',
-    //           // },{
-    //           //   label: 'Telefon:',
-    //           //   href: 'tel:+40722600000',
-    //           // }
-    //         ],
-    //       },
-    //     ],
-    //     copyright: `Copyright © ${new Date().getFullYear()} Softiwiz Creation srl`,
-    //   },
-    //   plugins: [
-    //     [
-    //       '@docusaurus/plugin-content-docs',
-    //       {
-    //         id: 'my-docs',
-    //         path: 'docs',
-    //         sidebarPath: require.resolve('./sidebars.js'),
-    //         editUrl: 'https://github.com/<org>/<repo>/edit/main/docs/',
-    //         routeBasePath: 'docs',
-    //         showLastUpdateAuthor: true,
-    //         showLastUpdateTime: true,
-    //       },
-    //     ],
-    //     [
-    //       '@docusaurus/plugin-content-pages',
-    //       {
-    //         path: 'pages',
-    //       },
-    //     ],
-    //     [
-    //       '@docusaurus/plugin-ideal-image',
-    //       {
-    //         sizes: [320, 640, 960, 1200, 1600, 2000],
-    //         quality: 70,
-    //         max: 2000,
-    //         min: 640,
-    //         steps: 6,
-    //       },
-    //     ],
-    //     [
-    //       '@docusaurus/plugin-content-docs',
-    //       {
-    //         id: 'withStaticFiles',
-    //         path: 'withStaticFiles',
-    //         editUrl: 'https://github.com/<org>/<repo>/edit/main/docs/',
-    //         sidebarPath: require.resolve('./sidebars.js'),
-    //         withStaticFiles: true, // Enable the withStaticFiles plugin
-    //       },
-    //     ],
-    //   ],
-    //   prism: {
-    //     theme: lightCodeTheme,
-    //     darkTheme: darkCodeTheme,
-    //   },
-    // }),
-};
+   };
 
 module.exports = config;
